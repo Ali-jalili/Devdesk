4a9bb16f73d09be5be35c248c4c221115758d8d5
Ali Jalili <mrali97jl@gmail.com>
2026-08-10 15:29:47 +0330

    feat: enhance Header component to display login/logout options based on user authentication

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,24 @@
 /** @format */
 
-import React from "react";
+import useAuth from "@/app/context/useAuth";
+
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
+  const { user, handleSignOut } = useAuth();
+
   return (
     <header>
       <h2>DevDesk</h2>
+
+      {user ? (
+        <button onClick={handleSignOut}>Logout</button>
+      ) : (
+        <div>
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/signup">Signup</NavLink>
+        </div>
+      )}
     </header>
   );
 }
