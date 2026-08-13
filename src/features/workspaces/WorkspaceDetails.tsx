ecd64b957b738f7d36ea5dba86442820c76642aa
Ali Jalili <mrali97jl@gmail.com>
2026-08-13 23:00:23 +0330

    feat: enhance WorkspaceDetails component with loading and error handling

--- src/features/workspaces/WorkspaceDetails.tsx
+++ src/features/workspaces/WorkspaceDetails.tsx
@@ -1,14 +1,16 @@
 /** @format */
 
 import useWorkspaceDetails from "@/app/hook/useWorkspaceDetails";
-import React from "react";
+import ErrorMessage from "@/components/ErrorMessage";
+import Loading from "@/ui/Loading";
 import { useParams } from "react-router-dom";
 
 function WorkspaceDetails() {
   const { workspaceId } = useParams<{ workspaceId: string }>();
 
   const { data, isLoading, error } = useWorkspaceDetails(workspaceId);
-
+  if (isLoading) return <Loading />;
+  if (error) return <ErrorMessage message={error.message} />;
   if (!workspaceId) return null;
 
   return <div>{data?.name}</div>;
