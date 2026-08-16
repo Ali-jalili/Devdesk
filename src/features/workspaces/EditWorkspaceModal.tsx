7d460c881c9e912f7a779b0058584095b4f42517
Ali Jalili <mrali97jl@gmail.com>
2026-08-16 16:53:38 +0330

    feat: implement form handling in EditWorkspaceModal and add useUpdateWorkspace hook

--- src/features/workspaces/EditWorkspaceModal.tsx
+++ src/features/workspaces/EditWorkspaceModal.tsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import { FiX } from "react-icons/fi";
+import { useForm } from "react-hook-form";
 
 interface EditWorkspaceModalProps {
   name: string;
@@ -13,6 +14,21 @@ function EditWorkspaceModal({
   description,
   onClose,
 }: EditWorkspaceModalProps) {
+  type FormData = {
+    name: string;
+    description: string;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  function submitFormModal(data: FormData) {
+    console.log(data);
+  }
+
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-background/10 backdrop-blur-md px-4">
       <div className="w-full max-w-lg rounded-2xl border border-border bg-card p-6 shadow-2xl">
@@ -39,7 +55,7 @@ function EditWorkspaceModal({
         </div>
 
         {/* Form UI */}
-        <div className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit(submitFormModal)}>
           <div>
             <label
               htmlFor="workspace-name"
@@ -49,13 +65,21 @@ function EditWorkspaceModal({
             </label>
 
             <input
+              {...register("name", {
+                required: {
+                  value: true,
+                  message: "Workspace name is required.",
+                },
+              })}
               id="workspace-name"
               type="text"
               defaultValue={name}
               className="w-full rounded-lg border border-border bg-background px-3 py-2.5 text-sm text-foreground outline-none transition focus:border-primary focus:ring-2 focus:ring-primary/20"
             />
           </div>
-
+          {errors.name && (
+            <p className="mt-2 text-sm text-red-500"> {errors.name.message}</p>
+          )}
           <div>
             <label
               htmlFor="workspace-description"
@@ -65,31 +89,44 @@ function EditWorkspaceModal({
             </label>
 
             <textarea
+              {...register("description", {
+                required: {
+                  value: true,
+                  message: "Workspace description is required.",
+                },
+              })}
               id="workspace-description"
               defaultValue={description}
               rows={4}
               className="w-full resize-none rounded-lg border border-border bg-background px-3 py-2.5 text-sm text-foreground outline-none transition focus:border-primary focus:ring-2 focus:ring-primary/20"
             />
+
+            {errors.description && (
+              <p className="mt-2 text-sm text-red-500">
+                {" "}
+                {errors.description.message}
+              </p>
+            )}
           </div>
-        </div>
 
-        {/* Actions */}
-        <div className="mt-7 flex justify-end gap-3">
-          <button
-            type="button"
-            onClick={onClose}
-            className="cursor-pointer rounded-lg border border-border px-4 py-2.5 text-sm font-medium text-foreground transition-colors hover:bg-muted"
-          >
-            Cancel
-          </button>
+          {/* Actions */}
+          <div className="mt-7 flex justify-end gap-3">
+            <button
+              type="button"
+              onClick={onClose}
+              className="cursor-pointer rounded-lg border border-border px-4 py-2.5 text-sm font-medium text-foreground transition-colors hover:bg-muted"
+            >
+              Cancel
+            </button>
 
-          <button
-            type="button"
-            className="cursor-pointer rounded-lg bg-primary px-4 py-2.5 text-sm font-medium text-primary-foreground transition-opacity hover:opacity-90"
-          >
-            Save changes
-          </button>
-        </div>
+            <button
+              type="submit"
+              className="cursor-pointer rounded-lg bg-primary px-4 py-2.5 text-sm font-medium text-primary-foreground transition-opacity hover:opacity-90"
+            >
+              Save changes
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
