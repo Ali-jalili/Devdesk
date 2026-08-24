14920d6970e3fe06c0647061b52e861ae49323d5
Ali Jalili <mrali97jl@gmail.com>
2026-08-24 15:39:35 +0330

    feat: implement createRequest function for handling API requests

---
+++ src/features/requests/RequestService.ts
@@ -0,0 +1,38 @@
+/** @format */
+
+import supabase from "@/lib/supabase";
+
+async function createRequest({
+  name,
+  method,
+  url,
+  headers,
+  params,
+  body,
+  description,
+  collectionId,
+}: {
+  name: string;
+  method: string;
+  url: string;
+  headers: Record<string, string>;
+  params: Record<string, string>;
+  body: Record<string, unknown>;
+  description: string;
+  collectionId: string;
+}) {
+  const { data, error } = await supabase.from("requests").insert({
+    name: name,
+    method: method,
+    url: url,
+    headers: headers,
+    params: params,
+    body: body,
+    description: description,
+    collection_id: collectionId,
+  });
+
+  return { data, error };
+}
+
+export { createRequest };
