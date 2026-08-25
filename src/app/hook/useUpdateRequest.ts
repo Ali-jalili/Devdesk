f700e951871cd6cdf3d084f09ed11cadca55008c
Ali Jalili <mrali97jl@gmail.com>
2026-08-25 15:12:52 +0330

    feat: implement useUpdateRequest hook for updating requests

---
+++ src/app/hook/useUpdateRequest.ts
@@ -0,0 +1,34 @@
+/** @format */
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateRequest } from "@/features/requests/RequestService";
+
+function useUpdateRequest() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      requestId,
+      data,
+    }: {
+      requestId: string;
+      data: {
+        name: string;
+        method: string;
+        url: string;
+        headers: Record<string, string>;
+        params: Record<string, string>;
+        body: string;
+        description: string;
+      };
+    }) => updateRequest(requestId, data),
+
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["requests", data.id],
+      });
+    },
+  });
+}
+
+export default useUpdateRequest;
